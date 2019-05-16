ec50d3bfb0065df905d83f9972a812eb606c722b
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-05-16 06:04:10 +0530

    add installer script

---
+++ goinstaller.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+GOVERSION=1.12.5
+
+GOROOT="$HOME/go"
+
+if [ ! -f "$GOROOT/bin/go" ]; then
+  echo "Installing Go..."
+
+  _GO_ARCHIVE="$HOME/go.tar.gz"
+
+  if [[ "$(uname)" =~ .*[Dd]arwin.* ]]; then
+    curl -o "$_GO_ARCHIVE" https://dl.google.com/go/go${GOVERSION}.darwin-amd64.tar.gz
+  else
+    curl -o "$_GO_ARCHIVE" https://dl.google.com/go/go${GOVERSION}.linux-amd64.tar.gz
+  fi
+
+  rm -rf "$GOROOT" &>/dev/null
+  tar -xzf "$_GO_ARCHIVE" -C "$HOME"
+fi
+
+GOPATH=$GOROOT
+PATH="$PATH:$GOROOT/bin"
+
+echo >> ~/.bashrc
+echo "# set environment variables for Go if it exists." >> ~/.bashrc
+echo 'if [ -d "$HOME/go/bin" ] ; then' >> ~/.bashrc
+echo "    GOROOT=\"$GOROOT\"" >> ~/.bashrc
+echo '    GOPATH="$GOROOT"' >> ~/.bashrc
+echo '    PATH="$PATH:$GOROOT/bin"' >> ~/.bashrc
+echo "fi" >> ~/.bashrc
+
+echo "Successfully installed Go!"
